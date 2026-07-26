0c4f07d8ff6e5bc28f6cf90b606290b2625819b3
Kim Morrison <kim@tqft.net>
2026-07-26 23:01:18 +0000

    feat(Ch7): expose enriched representability endpoints

--- EtingofRepresentationTheory/Chapter7.lean
+++ EtingofRepresentationTheory/Chapter7.lean
@@ -22,6 +22,7 @@ import EtingofRepresentationTheory.Chapter7.Introduction_7_4
 import EtingofRepresentationTheory.Chapter7.Example7_1_3
 import EtingofRepresentationTheory.Chapter7.Example7_1_5
 import EtingofRepresentationTheory.Chapter7.Example7_1_6
+import EtingofRepresentationTheory.Chapter7.EnrichedRepresentability
 import EtingofRepresentationTheory.Chapter7.SymmetricPowerFunctor
 import EtingofRepresentationTheory.Chapter7.SchurFunctor
 import EtingofRepresentationTheory.Chapter7.Example7_2_2
