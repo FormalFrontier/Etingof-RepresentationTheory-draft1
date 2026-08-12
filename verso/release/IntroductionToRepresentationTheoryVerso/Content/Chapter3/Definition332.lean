7a23ac84012f54a68d4169bf5584c85f6a4da69c
ff-agent <ff-agent@ff-server>
2026-08-12 14:25:27 +0000

    Sync panel for dual opposite action

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter3/Definition332.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter3/Definition332.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -17,3 +18,19 @@ number := false
 **Definition 3.3.2** (Dual representation). Let $`V` be a representation of any algebra $`A`. Then the **dual representation** $`V^*` is the representation of the opposite algebra $`A^{\mathrm{op}}` (or, equivalently, right $`A`-module) with the action
 
 $$`(f \cdot a)(v) := f(av).`
+
+## Formalization
+%%%
+tag := "Chapter3/Definition3.3.2/formalization"
+number := false
+%%%
+
+### Primary declarations
+
+{Manual.docstring RepresentationTheory.Module.DualOppositeAction.dualMulOppositeModule}
+
+### Supporting declarations
+
+{Manual.docstring RepresentationTheory.Module.DualOppositeAction.ParameterizedModuleType}
+
+{Manual.docstring RepresentationTheory.Module.DualOppositeAction.dualMulOppositeSMul}
