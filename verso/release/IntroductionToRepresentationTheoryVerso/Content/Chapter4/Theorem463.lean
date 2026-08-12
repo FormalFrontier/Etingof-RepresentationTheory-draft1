d86b1b29b791abd1c0337c7dbe0a4baacf420078
ff-agent <ff-agent@ff-server>
2026-08-12 14:51:34 +0000

    Sync panel for invariant complements

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter4/Theorem463.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter4/Theorem463.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -19,3 +20,13 @@ number := false
 **Theorem 4.6.3.** _A finite dimensional unitary representation $`V` of any group $`G` is completely reducible._
 
 **Proof.** Let $`W` be a subrepresentation of $`V`. Let $`W^\perp` be the orthogonal complement of $`W` in $`V` under the Hermitian inner product. Then $`W^\perp` is a subrepresentation of $`V`, and $`V = W \oplus W^\perp`. This implies that $`V` is completely reducible. $`\square`
+
+## Formalization
+%%%
+tag := "Chapter4/Theorem4.6.3/formalization"
+number := false
+%%%
+
+### Primary declarations
+
+{Manual.docstring RepresentationTheory.InvariantComplements.exists_invariant_isCompl_of_preserves_inner}
