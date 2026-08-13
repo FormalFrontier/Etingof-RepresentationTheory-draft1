6bb0fdad7b956c5ac19ddb3d9fc2782999df3651
ff-agent <ff-agent@ff-server>
2026-08-13 20:26:07 +0000

    Migrate conjugacy class trace maps

--- clean-code/release/RepresentationTheory.lean
+++ clean-code/release/RepresentationTheory.lean
@@ -281,6 +281,7 @@ import RepresentationTheory.CategoryTheory.ProjectiveResolution.ShortComplex
 import RepresentationTheory.FinsuppDualTensor
 import RepresentationTheory.GroupTheory.ZMod.ComplexCharacters
 import RepresentationTheory.Algebra.Module.SimpleMatrixCoordinates
+import RepresentationTheory.ConjugacyClassTrace
 
 /-!
 # Representation theory formalization
