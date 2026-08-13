78413c1e19eb15561980cdc137a9ab352b3c214d
ff-agent <ff-agent@ff-server>
2026-08-13 21:43:07 +0000

    Sync projective resolution formalization panel

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter8/Exercise822.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter8/Exercise822.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -17,3 +18,19 @@ number := false
 %%%
 
 *Exercise 8.2.2.* Show that any module has a projective resolution (for example, one consisting of free modules).
+
+## Formalization
+%%%
+tag := "Chapter8/Exercise8.2.2/formalization"
+number := false
+%%%
+
+### Primary declarations
+
+{Manual.docstring RepresentationTheory.CategoryTheory.Abelian.ProjectiveResolution.exists_free_projectiveResolution}
+
+{Manual.docstring RepresentationTheory.CategoryTheory.Abelian.ProjectiveResolution.nonempty_projectiveResolution}
+
+### Supporting declarations
+
+{Manual.docstring RepresentationTheory.CategoryTheory.Abelian.ProjectiveResolution.freeProjectiveResolution}
