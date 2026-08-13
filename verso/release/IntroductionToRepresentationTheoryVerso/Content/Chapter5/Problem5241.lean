af40ddc8d5986271b4efe1f4fd8bddae56063307
ff-agent <ff-agent@ff-server>
2026-08-13 21:57:21 +0000

    Sync partition submodules formalization panel

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter5/Problem5241.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter5/Problem5241.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -25,3 +26,15 @@ is isomorphic to $`V_\lambda`.
 Hint: Define $`S_n`-homomorphisms $`f : V_\lambda \to V'_\lambda` and $`g : V'_\lambda \to V_\lambda` by the formulas $`f(x) = x a_\lambda` and $`g(y) = y b_\lambda`, and show that they are inverse to each other up to a nonzero scalar.
 
 (b) Let $`\phi : \mathbb{C}[S_n] \to \mathbb{C}[S_n]` be the automorphism sending $`s` to $`(-1)^s s` for any permutation $`s`. Show that $`\phi` maps any representation $`V` of $`S_n` to $`V \otimes \mathbb{C}_-`. Show also that $`\phi(\mathbb{C}[S_n] a) = \mathbb{C}[S_n] \phi(a)`, for $`a \in \mathbb{C}[S_n]`. Use (a) to deduce that $`V_\lambda \otimes \mathbb{C}_- = V_{\lambda^*}`, where $`\lambda^*` is the conjugate partition to $`\lambda`, obtained by reflecting the Young diagram of $`\lambda`.
+
+## Formalization
+%%%
+tag := "Chapter5/Problem5.24.1/formalization"
+number := false
+%%%
+
+### Supporting declarations
+
+{Manual.docstring RepresentationTheory.SymmetricGroup.PartitionSubmodules.exists_equivariantMap}
+
+{Manual.docstring RepresentationTheory.SymmetricGroup.PartitionSubmodules.partitionSubmodule}
