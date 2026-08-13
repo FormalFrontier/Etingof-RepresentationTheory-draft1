54b95e85d0f358d2e8ac2e6142e9d4e67bad5b91
ff-agent <ff-agent@ff-server>
2026-08-13 00:40:13 +0000

    Synchronize formalization panels through module 0286

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter5/Definition5231.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter5/Definition5231.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -17,3 +18,19 @@ number := false
 %%%
 
 *Definition 5.23.1.* We say that a finite dimensional representation $`Y` of $`GL(V)` is *algebraic* (or *rational*, or *polynomial*) if its matrix elements are polynomial functions of the entries of $`g`, $`g^{-1}`, $`g \in GL(V)` (i.e., belong to $`k[g_{ij}][1/\det(g)]`).
+
+## Formalization
+%%%
+tag := "Chapter5/Definition5.23.1/formalization"
+number := false
+%%%
+
+### Supporting declarations
+
+{Manual.docstring RepresentationTheory.GeneralLinearGroup.Auxiliary.HasAuxiliaryMapProperty}
+
+{Manual.docstring RepresentationTheory.GeneralLinearGroup.Auxiliary.HasAuxiliaryRepresentationProperty}
+
+{Manual.docstring RepresentationTheory.GeneralLinearGroup.Auxiliary.auxiliaryRepresentationProperty_iff_mapProperty}
+
+{Manual.docstring RepresentationTheory.GeneralLinearGroup.Auxiliary.exists_auxiliaryMap_ne_id_at_one}
