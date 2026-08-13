0081de3825d02a763b4aa31fcda282f6f872dcf0
ff-agent <ff-agent@ff-server>
2026-08-13 05:12:21 +0000

    Use neutral release header for subgroup Hom adjunction

--- clean-code/release/RepresentationTheory/Subgroup/HomAdjunction.lean
+++ clean-code/release/RepresentationTheory/Subgroup/HomAdjunction.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 Etingof Formalization Project. All rights reserved.
+Copyright (c) 2026 FormalFrontier. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENCE.
-Authors: Etingof Formalization Project
+Authors: FormalFrontier
 -/
 import Mathlib
 import RepresentationTheory.Alignment.Attribute
