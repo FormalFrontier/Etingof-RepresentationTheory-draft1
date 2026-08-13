86bb66d8101e435c673dfbc50fc267fc9b787a35
ff-agent <ff-agent@ff-server>
2026-08-13 18:46:57 +0000

    Synchronize Example 7.9.2 formalization panel

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter7/Example792.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter7/Example792.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -17,3 +18,23 @@ number := false
 %%%
 
 *Example 7.9.2.* The functors $`\operatorname{Ind}_K^G`, $`\operatorname{Res}_K^G`, $`\operatorname{Hom}_G(V, ?)` in the theory of group representations over a field $`k` are additive and $`k`-linear.
+
+## Formalization
+%%%
+tag := "Chapter7/Example7.9.2/formalization"
+number := false
+%%%
+
+### Primary declarations
+
+{Manual.docstring RepresentationTheory.CategoryTheory.LinearFunctors.indFunctor_additive}
+
+{Manual.docstring RepresentationTheory.CategoryTheory.LinearFunctors.indFunctor_linear}
+
+{Manual.docstring RepresentationTheory.CategoryTheory.LinearFunctors.linearCoyoneda_obj_additive}
+
+{Manual.docstring RepresentationTheory.CategoryTheory.LinearFunctors.linearCoyoneda_obj_linear}
+
+{Manual.docstring RepresentationTheory.CategoryTheory.LinearFunctors.resFunctor_additive}
+
+{Manual.docstring RepresentationTheory.CategoryTheory.LinearFunctors.resFunctor_linear}
