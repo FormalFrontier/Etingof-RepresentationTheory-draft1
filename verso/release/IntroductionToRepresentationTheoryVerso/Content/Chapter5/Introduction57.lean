e73fa8d7a18f18d95c4998b7451e5b959c8bb837
ff-agent <ff-agent@ff-server>
2026-08-14 00:02:20 +0000

    Synchronize virtual representation formalization panels

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter5/Introduction57.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter5/Introduction57.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -20,3 +21,13 @@ number := false
 %%%
 tag := "Chapter5/Introduction_5.7/heading-1"
 %%%
+
+## Formalization
+%%%
+tag := "Chapter5/Introduction_5.7/formalization"
+number := false
+%%%
+
+### Supporting declarations
+
+{Manual.docstring RepresentationTheory.VirtualRepresentations.Basic.VirtualRepresentation}
