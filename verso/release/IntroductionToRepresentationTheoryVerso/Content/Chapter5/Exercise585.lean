827b1aac3186c1a6d14b313f2b498ed8854f80e1
ff-agent <ff-agent@ff-server>
2026-08-14 03:32:20 +0000

    Synchronize Exercise 5.8.5 formalization panel

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter5/Exercise585.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter5/Exercise585.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -21,3 +22,19 @@ number := false
 $$`e_\chi = \frac{1}{|K|} \sum_{g \in K} \chi(g)^{-1} g \in \mathbb{C}[K]`
 
 be the idempotent corresponding to $`\chi`. Show that the $`G`-representation $`\operatorname{Ind}_K^G \mathbb{C}_\chi` is naturally isomorphic to $`\mathbb{C}[G] e_\chi` (with $`G` acting by left multiplication).
+
+## Formalization
+%%%
+tag := "Chapter5/Exercise5.8.5/formalization"
+number := false
+%%%
+
+### Supporting declarations
+
+{Manual.docstring RepresentationTheory.SubgroupCharacters.auxiliary_equivariant_map_of_subgroup_character}
+
+{Manual.docstring RepresentationTheory.SubgroupCharacters.groupAlgebraElementOfSubgroupCharacter}
+
+{Manual.docstring RepresentationTheory.SubgroupCharacters.representationOfSubgroupCharacter}
+
+{Manual.docstring RepresentationTheory.SubgroupCharacters.submoduleOfSubgroupCharacter}
