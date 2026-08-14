600f7706b8828b4a7ed3790cb7e04bcfd02fad8e
ff-agent <ff-agent@ff-server>
2026-08-14 03:44:57 +0000

    Synchronize Corollary 4.2.4 formalization panel

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter4/Corollary424.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter4/Corollary424.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -17,3 +18,15 @@ number := false
 %%%
 
 **Corollary 4.2.4.** _Any finite dimensional representation of $`G` is determined by its character if $`k` has characteristic $`0`; namely, $`\chi_V = \chi_W` implies $`V \cong W`._
+
+## Formalization
+%%%
+tag := "Chapter4/Corollary4.2.4/formalization"
+number := false
+%%%
+
+### Primary declarations
+
+{Manual.docstring RepresentationTheory.Group.CharacterAuxiliary.complex_iso_of_character_eq}
+
+{Manual.docstring RepresentationTheory.Group.CharacterAuxiliary.iso_of_character_eq}
