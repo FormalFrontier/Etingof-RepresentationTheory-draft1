6476c7a2fcfcb168c011b56dc64f024e3b55a9e7
ff-agent <ff-agent@ff-server>
2026-08-14 07:20:59 +0000

    Synchronize Example 8.1.7 formalization panel

--- verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter8/Example817.lean
+++ verso/release/IntroductionToRepresentationTheoryVerso/Content/Chapter8/Example817.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 American Mathematical Society. All rights reserved.
 -/
 
 import VersoManual
+import RepresentationTheory
 
 open Verso.Genre Manual
 
@@ -17,3 +18,13 @@ number := false
 %%%
 
 *Example 8.1.7.* Let $`A` be an algebra and $`P` be a left $`A`-module. Then $`P` is projective if and only if $`P^*` is an injective right $`A`-module.
+
+## Formalization
+%%%
+tag := "Chapter8/Example8.1.7/formalization"
+number := false
+%%%
+
+### Primary declarations
+
+{Manual.docstring RepresentationTheory.Algebra.Module.Duality.Module.projective_iff_dual_injective}
