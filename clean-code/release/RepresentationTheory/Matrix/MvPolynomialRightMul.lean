1ca014c968bc3412007ad942eef8f1659550a41a
ff-agent <ff-agent@ff-server>
2026-08-14 11:01:21 +0000

    Remove unnecessary matrix-polynomial linter option

--- clean-code/release/RepresentationTheory/Matrix/MvPolynomialRightMul.lean
+++ clean-code/release/RepresentationTheory/Matrix/MvPolynomialRightMul.lean
@@ -1,7 +1,6 @@
 import Mathlib
 import RepresentationTheory.Auxiliary.GeneralLinearCoordinateLocalization
 
-set_option linter.style.longLine false
 set_option linter.dupNamespace false
 
 namespace RepresentationTheory.Matrix.MvPolynomialRightMul.Matrix
