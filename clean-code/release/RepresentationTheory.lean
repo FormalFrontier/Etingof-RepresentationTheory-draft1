c77efd10baeffb768d4650e98d3945919affc0f1
ff-agent <ff-agent@ff-server>
2026-08-14 09:11:23 +0000

    Integrate homological tensor product migration

--- clean-code/release/RepresentationTheory.lean
+++ clean-code/release/RepresentationTheory.lean
@@ -322,6 +322,7 @@ import RepresentationTheory.FGModuleCat.SubobjectOrder
 import RepresentationTheory.FDRep.Biproduct
 import RepresentationTheory.ModuleCat.Auxiliary
 import RepresentationTheory.Algebra.Homology.TensorProductConstruction
+import RepresentationTheory.Algebra.Homology.TensorProduct
 import RepresentationTheory.PiTensorProduct.Constructions
 import RepresentationTheory.TensorProduct.AuxiliaryScalarAction
 import RepresentationTheory.TensorProduct.BalancedRelations
